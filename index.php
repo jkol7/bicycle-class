d3a86d9da4ff1132b43d0a22de3270d2cb86637f
jkol7 <jkolcun7@gmail.com>
2022-11-02 20:07:22 -0400

    Ride button

--- index.php
+++ index.php
@@ -17,10 +17,18 @@
     <!-- Form that sets parameters for function that initializes Bike object -->
 
 <form action="" method="get">
+
     Bike Color: <input type="text" name="bikeColor">
     <p></p>
-    Bike Size: <input type="text" name="bikeSize">
+    <label for="bikeSize">Bike Size?</label>
+        <select id="bikeSize" name="bikeSize">
+        <option value="tiny">Tiny</option>
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+        </select>
     <p></p>
+
     <input type="checkbox" id="frame" name="bikeFrame" value=true>
     <label for="frame"> Add a frame</label><br>
     <input type="checkbox" id="handlebars" name="bikeHandlebars" value=true>
@@ -30,33 +38,19 @@
     <input type="checkbox" id="seat" name="bikeSeat" value=true>
     <label for="seat"> Add a seat</label><br>
     <p></p>
-    <label for="wheels">How many wheels?</label>
-        <select id="bikeWheelSelect" name="bikeWheels">
+
+    <label for="bikeWheels">Bike Wheels?</label>
+        <select id="bikeWheels" name="bikeWheels">
         <option value=0>0</option>
-        <option value=1>1</option>
         <option value=2>2</option>
         <option value=3>3</option>
         <option value=4>4</option>
         </select>
     <p></p>
-    <label for="gear">What gear?</label>
-        <select id="bikeGear" name="bikeGear">
-        <option value=0>0</option>
-        <option value=1>1</option>
-        <option value=3>3</option>
-        <option value=21>21</option>
-        <option value=24>24</option>
-        <option value=27>27</option>
-        </select>
-    <p></p>
-    <label for="pedalSpeed">Pedal speed?</label>
-        <select id="pedalSpeed" name="pedalSpeed">
-        <option value="slow">Slow</option>
-        <option value="medium">Medium</option>
-        <option value="fast">Fast</option>
-        </select>
-    <p></p>
-<input type="submit" value="submit" name="submit"> </form>
+<input type="submit" value="Create Bike" name="submit"> </form>
+
+
+<p></p>
 
 
 
@@ -65,18 +59,27 @@
         function createBike($bikeColor, $bikeSize, $bikeFrame, $bikeHandlebars, $bikeCrank, $bikeSeat, $bikeWheels){
             $newBike = new Bicycle($bikeColor, $bikeSize, $bikeFrame, $bikeHandlebars, $bikeCrank, $bikeSeat, $bikeWheels);
 
-
-            echo $newBike->rideBike("slow");
-
-
+            if(isset($_POST['button1'])) {
+                $newBike->rideBike("slow");
+            }
+            if(isset($_POST['button2'])) {
+                echo "This is Button2 that is selected";
+            }
         }
 
         if(isset($_GET['submit']))
             {
-            createBike($_GET["bikeColor"],$_GET["bikeSize"], 
-            $_GET["bikeFrame"], $_GET["bikeHandlebars"], $_GET["bikeCrank"],
-            $_GET["bikeSeat"], $_GET["bikeWheels"]);
-            } 
+            createBike(
+            $_GET["bikeColor"] ?? $bikeColor = "grey",
+            $_GET["bikeSize"] ?? $bikeSize = "medium", 
+            $_GET["bikeFrame"] ?? $bikeFrame = "false", 
+            $_GET["bikeHandlebars"] ?? $bikeHandlebars = "false", 
+            $_GET["bikeCrank"] ?? $bikeCrank = "false", 
+            $_GET["bikeSeat"] ?? $bikeSeat = "false", 
+            $_GET["bikeWheels"] ?? $bikeWheels = 2);
+            }
+
+            
     ?>
 </body>
 </html>
